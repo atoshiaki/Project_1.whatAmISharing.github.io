cf850873fc5802d15a67a44b7354054d44e01522
korynelson <66579570+korynelson@users.noreply.github.com>
2020-07-29 19:04:25 -0700

    Api key (#29)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete Index.html
    
    Irrelevant file
    
    * Add files via upload
    
    Co-authored-by: atoshiaki <66647837+atoshiaki@users.noreply.github.com>

--- Project_1/script.js
+++ Project_1/script.js
@@ -1,115 +1,52 @@
-  function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://YourRedirectUri.com/callback?code=zWrT1GkdshSadIowJW0Rm4w2kKhOzv1W";
-  
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "api-key": "" };
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
-  
-    // If the user provides a zipcode, include it in the queryParams object
-    var zipcode = $("#")
-      .val()
-      .trim();
-  
-    if (parseInt(zipcode)) {
-      queryParams. ;
-    }
-// All Kroger
-    // Reference
-    https://developer.kroger.com/reference/#operation/authorizationCode
-    // api
-    https://api.kroger.com/v1/connect/oauth2/authorize?scope={{SCOPES}}&response_type=code&client_id={{CLIENT_ID}}&redirect_uri={{REDIRECT_URI}}
-    // api json
-    https://YourRedirectUri.com/callback?code=zWrT1GkdshSadIowJW0Rm4w2kKhOzv1W
-    //certification
-    https://api-ce.kroger.com/v1/connect/oauth2/authorize
-    //Production Envionment
-    https://api.kroger.com/v1/connect/oauth2/authorize
-    {
-      "data": {
-        "productId": "0001111041700",
-        "aisleLocations": [
-          {
-            "bayNumber": "13",
-            "description": "Aisle 35",
-            "number": "35",
-            "numberOfFacings": "5",
-            "sequenceNumber": "3",
-            "side": "L",
-            "shelfNumber": "2",
-            "shelfPositionInBay": "1"
-          }
-        ],
-        "brand": "Kroger",
-        "categories": [
-          "Dairy"
-        ],
-        "countryOrigin": "United States",
-        "description": "Kroger 2% Reduced Fat Milk",
-        "items": [
-          {
-            "itemId": "0001111041700",
-            "favorite": true,
-            "fulfillment": {
-              "curbside": true,
-              "delivery": true,
-              "instore": true,
-              "shiptohome": true
-            },
-            "price": {
-              "regular": 1.99,
-              "promo": 1.59,
-              "regularPerUnitEstimate": 1.99,
-              "promoPerUnitEstimate": 1.59
-            },
-            "nationalPrice": {
-              "regular": 1.99,
-              "promo": 1.59,
-              "regularPerUnitEstimate": 1.99,
-              "promoPerUnitEstimate": 1.59
-            },
-            "size": "1 gal",
-            "soldBy": "unit"
-          }
-        ],
-        "itemInformation": {
-          "depth": "3.5",
-          "height": "2.0",
-          "width": "4.75"
-        },
-        "temperature": {
-          "indicator": "Refrigerated",
-          "heatSensitive": true
-        },
-        "images": [
-          {
-            "id": "string",
-            "perspective": "front",
-            "default": true,
-            "sizes": [
-              {
-                "id": "7df2d0a3-8349-44d4-9512-1dab89e675a9",
-                "size": "medium",
-                "url": "https://www.kroger.com/product/images/medium/front/0001111041700"
-              }
-            ]
-          }
-        ],
-        "upc": "0001111041700"
-      },
-      "meta": {
-        "pagination": {
-          "total": 0,
-          "start": 0,
-          "limit": 0
-        },
-        "warnings": [
-          "string"
-        ]
-      }
-    }
+// Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAN1HuZR2h3OStGDn61coErkA7k4QHfrbI&callback=initMap';
+script.defer = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function prompt() {
+  // JS API is loaded and available
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
+  // Note: This example requires that you consent to location sharing when
+  // prompted by your browser. If you see the error "The Geolocation service
+  // failed.", it means you probably did not give permission for the browser to
+  // locate you.
+let map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
